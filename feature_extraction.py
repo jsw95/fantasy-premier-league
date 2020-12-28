7a7ea840a2c75b02f2fb93484ad1029362b8f3b2
Jack Wells <wells.jack@hotmail.co.uk>
2020-12-28 12:25:25 +0000

    broken gradients

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,3 +5,4 @@ from config import Config
 def proportion_of_match_played(minutes):
     return minutes / Config.MINUTES_IN_A_FOOTBALL_MATCH
 
+
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 import keras
+from sklearn.preprocessing import MinMaxScaler
 from config import Config
 import glob
 
@@ -34,6 +35,8 @@ for i, player_name in enumerate(os.listdir(f"data/{season}/players/")):
     player_features = player_data[features]
     if player_features.empty:
         continue
+
+
     target_points[-len(player_data):] = player_data['points_gw_t+1']
     gw_features[-len(player_data):] = player_features.values
     all_player_target_points[i, :] = target_points
@@ -44,27 +47,44 @@ for i, player_name in enumerate(os.listdir(f"data/{season}/players/")):
     # print(len(player_data))
     # print()
 
+
+
 X_train, X_test, y_train, y_test = train_test_split(all_player_features, all_player_target_points, test_size=0.1)
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.2)
+
+scaler = MinMaxScaler()
+_X_train = X_train.reshape((len(X_train) * n_gws, dimensionality))
+_X_valid = X_valid.reshape((len(X_valid) * n_gws, dimensionality))
+_X_test = X_test.reshape((len(X_test) * n_gws, dimensionality))
+scaler = scaler.fit(_X_train)
+normalized_X_train = scaler.transform(_X_train)
+normalized_X_valid = scaler.transform(_X_valid)
+normalized_X_test = scaler.transform(_X_test)
+normalized_X_train = normalized_X_train.reshape((len(X_train), n_gws, dimensionality))
+normalized_X_valid = normalized_X_valid.reshape((len(X_valid), n_gws, dimensionality))
+normalized_X_test = normalized_X_test.reshape((len(X_test), n_gws, dimensionality))
+
+inversed = scaler.inverse_transform(normalized_X_train)
+inversed = inversed.reshape((len(X_train), n_gws, dimensionality))
 
 y_pred = X_valid[:, :, -1]
 print(f"Last Value MSE: {np.mean(keras.losses.mean_squared_error(y_valid, y_pred))}")
 
 model_dense = keras.models.Sequential([
     keras.layers.Flatten(input_shape=[n_gws * dimensionality, 1]),
-    keras.layers.Dense(1)
+    keras.layers.Dense(1,activation='relu')
 ])
 
 model_dense.compile(loss="mean_squared_error",
                     # optimizer=keras.optimizers.SGD(learning_rate=0.01),
                     metrics=["accuracy"])
 
-model_dense.fit(X_train, y_train, epochs=100, validation_data=(X_valid, y_valid))
-y_pred_dense = model_dense.predict(X_valid)[:, np.newaxis, :].astype(int)
-print(f"Dense NN MSE: {np.mean(keras.losses.mean_squared_error(y_valid, y_pred_dense))}")
+model_dense.fit(normalized_X_train, y_train, epochs=100, validation_data=(normalized_X_valid, y_valid))
+y_pred_dense = model_dense.predict(normalized_X_test).astype(int)
+print(f"Dense NN MSE: {np.mean(keras.losses.mean_squared_error(y_valid[:, np.newaxis, :], y_pred_dense))}")
 
 model_rnn = keras.models.Sequential([
-    keras.layers.SimpleRNN(50, return_sequences=True, input_shape=[n_gws, dimensionality]),
+    keras.layers.SimpleRNN(10, return_sequences=True, input_shape=[n_gws, dimensionality]),
     # keras.layers.SimpleRNN(50),
     keras.layers.Dense(1)
 ])
@@ -73,9 +93,9 @@ model_rnn.compile(loss="mean_squared_error",
                   # optimizer=keras.optimizers.SGD(learning_rate=0.01),
                   metrics=["accuracy"])
 
-model_rnn.fit(X_train, y_train, epochs=100, validation_data=(X_valid, y_valid))
-y_pred_rnn = model_rnn.predict(X_valid)[:, np.newaxis, :].astype(int)
-print(f"RNN MSE: {np.mean(keras.losses.mean_squared_error(y_valid, y_pred_rnn))}")
+model_rnn.fit(normalized_X_train, y_train, epochs=10, validation_data=(normalized_X_valid, y_valid))
+y_pred_rnn = model_rnn.predict(normalized_X_valid).astype(int)
+print(f"RNN MSE: {np.mean(keras.losses.mean_squared_error(y_valid[:, :, np.newaxis], y_pred_rnn))}")
 
 n_plots = 5
 fig, axs = plt.subplots(n_plots)
