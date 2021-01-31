6bcd6c1551261bbdba4b9d73c22f72f3d10e3a1a
Jack Wells <wells.jack@hotmail.co.uk>
2021-01-31 08:50:15 +0000

    better team opt method

--- choice_optimisation.py
+++ choice_optimisation.py
@@ -8,29 +8,30 @@ def main(budget=200):
     Constraints:
         - Cost < budget (100m)
         - 2 GKP, 5 DEF, 5 MID, 3 FWD
+        - Max 3 from each team
+        - Player can only be used once
     """
 
     test_data = {
-        "Alice": {"cost": 10, "n_gkp": 1, "n_def": 0, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 2},
-        "Bob": {"cost": 20, "n_gkp": 1, "n_def": 0, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 0},
-        "Charlie": {"cost": 21, "n_gkp": 1, "n_def": 0, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 1},
-        "Dave": {"cost": 10, "n_gkp": 1, "n_def": 0, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 10},
-        "def1": {"cost": 10, "n_gkp": 0, "n_def": 1, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 10},
-        "def2": {"cost": 10, "n_gkp": 0, "n_def": 1, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 12},
-        "def3": {"cost": 10, "n_gkp": 0, "n_def": 1, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 12},
-        "def4": {"cost": 10, "n_gkp": 0, "n_def": 1, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 12},
-        "def5": {"cost": 10, "n_gkp": 0, "n_def": 1, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()), "points": 12},
-        "def6": {"cost": 10, "n_gkp": 0, "n_def": 1, "n_mid": 0, "n_fwd": 0, "id": str(uuid.uuid4()),"points": 12},
+        "Alice": {"cost": 10, "GKP": 1, "DEF": 0, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 2},
+        "Bob": {"cost": 20, "GKP": 1, "DEF": 0, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 0},
+        "Charlie": {"cost": 21, "GKP": 1, "DEF": 0, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 1},
+        "Dave": {"cost": 10, "GKP": 1, "DEF": 0, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 10},
+        "def1": {"cost": 10, "GKP": 0, "DEF": 1, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 10},
+        "def2": {"cost": 10, "GKP": 0, "DEF": 1, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 12},
+        "def3": {"cost": 10, "GKP": 0, "DEF": 1, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 12},
+        "def4": {"cost": 10, "GKP": 0, "DEF": 1, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 12},
+        "def5": {"cost": 10, "GKP": 0, "DEF": 1, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 12},
+        "def6": {"cost": 10, "GKP": 0, "DEF": 1, "MID": 0, "FWD": 0, "id": str(uuid.uuid4()), "points": 12},
     }
 
     constraint_dict = {
         "cost": {"min": 0, "max": budget},
-        "n_gkp": {"min": 2, "max": 2},
-        "n_def": {"min": 5, "max": 5},
-        # "n_mid": {"min": 5, "max": 5},
-        # "n_fwd": {"min": 3, "max": 3},
+        "GKP": {"min": 2, "max": 2},
+        "DEF": {"min": 5, "max": 5},
+        # "MID": {"min": 5, "max": 5},
+        # "FWD": {"min": 3, "max": 3},
     }
-
 
     # adding a unique id constraint to ensure players selected only once
     player_ids = [player['id'] for player in test_data.values()]
@@ -59,14 +60,20 @@ def main(budget=200):
         for name, player_data in test_data.items():
             constraint.SetCoefficient(players[name], player_data[constraint_name])
 
-
     status = solver.Solve()
     if status == solver.OPTIMAL:
-        price = 0
+        team_selection = {"GKP": [], "DEF": [], "MID": [], "FWD": []}
         for name, player_data in test_data.items():
-            price += players[name].solution_value()
+            selected = players[name].solution_value()
+            if selected:
+                for position in team_selection.keys():
+                    if player_data[position] == 1:
+                        team_selection[position].append(name)
+                        break
             print(name)
             print(players[name].solution_value())
             print()
+    print(team_selection)
+
 if __name__ == "__main__":
     main()
